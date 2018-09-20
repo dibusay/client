a9ec81dac2de8d404c5e8ad61d73eba382e1e451
shantidyah <shanti.dyah@gmail.com>
2018-09-20 18:54:43 +0700

    image picker & clarifai

--- MasakYuk/src/components/Main.js
+++ MasakYuk/src/components/Main.js
@@ -21,26 +21,7 @@ export default class Main extends Component{
     }
 }
 
-const MainNavigator = createBottomTabNavigator({
-    Home: {
-        screen: Home
-    },
-    Favourites: {
-        screen: Favourites
-    }
-}, {
-    animationEnabled: true,
-    swipeEnabled: true,
-    tabBarOptions:{
-        showIcon: true,
-        showLabel: false,
-        style:{
-            backgroundColor: '#ffc107'
-        },
-        activeTintColor: 'red',
-        inactiveTintColor: 'white'
-    }
-})
+
 
 // export default createBottomTabNavigator({
 //     Home: {
