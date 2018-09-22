c9c3d78ebf04cbaeb76ce6f6d5f2423d52ec9a52
Susan Nio <susan.wen88@gmail.com>
2018-09-22 18:14:00 +0700

    adding auth to firebase

---
+++ MasakYuk/src/components/Loading.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from 'react-native-firebase'
+
+export default class Loading extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'Login')
+    })
+  }
+
+  handleLoading = () => {
+    // TODO: Firebase stuff...
+    console.log('handleLoading')
+    this.props.navigation.navigate('Login');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
